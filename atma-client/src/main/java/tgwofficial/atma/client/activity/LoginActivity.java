9ee2d0a16ecff4d5d7b9c218ef156ccc9c2483fc
mib-iqbal <mib.iqbaal@gmail.com>
2018-08-20 21:57:41 +1000

    MI | Adding login,Main and detail Activity
    Signed-off-by: mib-iqbal <mib.iqbaal@gmail.com>

---
+++ atma-client/src/main/java/tgwofficial/atma/client/activity/LoginActivity.java
@@ -0,0 +1,85 @@
+package tgwofficial.atma.client.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
+import tgwofficial.atma.client.R;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+    EditText edtUsername;
+    EditText edtPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtUsername = (EditText) findViewById(R.id.email);
+        edtPassword = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.email_sign_in_button);
+      //  userService = ApiUtils.getUserService();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+                //validate form
+                if(validateLogin(username, password)){
+                    //do login
+                    doLogin(username, password);
+                }
+            }
+        });
+
+    }
+
+    private boolean validateLogin(String username, String password){
+        if(username == null || username.trim().length() == 0){
+            Toast.makeText(this, "Username is required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password == null || password.trim().length() == 0){
+            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void doLogin(final String username,final String password){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setBasicAuth(username, password);
+        client.get("http://192.168.1.4:5000/api/resource", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                System.out.println("SUCCESS!!!!!!!!!!");
+                Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable
+                    error)
+            {
+                error.printStackTrace(System.out);
+            }
+        });
+    }
+
+}
+
