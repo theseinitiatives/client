db8842164a05d0371e05d1b2675a6a87cdb5e2d0
mib-iqbal <mib.iqbaal@gmail.com>
2018-09-02 10:36:54 +1000

    MI | Updating and add Pojo for nested data
    Signed-off-by: mib-iqbal <mib.iqbaal@gmail.com>

--- atma-client/src/main/java/tgwofficial/atma/client/sync/RestApi.java
+++ atma-client/src/main/java/tgwofficial/atma/client/sync/RestApi.java
@@ -12,9 +12,11 @@ import com.loopj.android.http.TextHttpResponseHandler;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 import tgwofficial.atma.client.db.DbManager;
+import tgwofficial.atma.client.sync.model.IbuModel.Data;
 import tgwofficial.atma.client.sync.model.IbuModel.IdentitasIbuModel;
 
 public class RestApi {
@@ -49,13 +51,54 @@ public class RestApi {
 
     public void saveTodb(String  data, Integer statusCode) {
         Gson gson = new GsonBuilder().create();
-        IdentitasIbuModel[] videoArray = gson.fromJson(data, IdentitasIbuModel[].class);
+        IdentitasIbuModel[] DataArray = gson.fromJson(data, IdentitasIbuModel[].class);
+      //  List<IdentitasIbuModel> DataList = Arrays.asList(DataArray);
+        List<IdentitasIbuModel> DataList = new ArrayList<>(Arrays.asList(DataArray));
 
-        Log.d("ssssssssssssssss", ""+statusCode);
-        Log.d("aaaaaaaaaaaaaaaaa", Arrays.toString(videoArray));
+         Log.d("ssssssssssssssss", ""+data);
+       // Log.d("aaaaaaaaaaaaaaaaa", Arrays.toString(DataArray));
 
 
+        for(IdentitasIbuModel model : DataList) {
+            Log.i("getupdate_id" ,model.getupdate_id());
+            Log.i("getform_name" ,model.getform_name());
+            Log.i("getlocation_id" ,model.getlocation_id());
+            Log.i("getuser_id" ,model.getuser_id());
+            Log.i("getData" ,model.getData());
+            String data_ = "["+model.getData()+"]";
+            Log.i("aaaaaaaaaaaaaaaaa" ,data_);
+            Data[] Datas = gson.fromJson(data_, Data[].class);
+            List<Data> DataListed = new ArrayList<>(Arrays.asList(Datas));
+
+            for(Data ListData : DataListed) {
+                Log.i("getName" ,ListData.getName());
+                Log.i("getSpousename" ,ListData.getSpousename());
+                Log.i("getStatus" ,ListData.getStatus());
+                Log.i("getDusun" ,ListData.getDusun());
+                Log.i("getHpht" ,ListData.getHpht());
+                Log.i("getHtp" ,ListData.getHtp());
+                Log.i("getTglLahir" ,ListData.getTglLahir());
+                Log.i("getTelp" ,ListData.getTelp());
+                Log.i("getTglPersalinan" ,ListData.getTglPersalinan());
+                Log.i("getKondisiIbu" ,ListData.getKondisiIbu());
+                Log.i("getKondisiAnak" ,ListData.getKondisiAnak());
+                Log.i("getKader" ,ListData.getKader());
+                Log.i("getIsSend" ,ListData.getIsSend());
+                Log.i("getIsSync" ,ListData.getIsSync());
+                Log.i("getTimestamp" ,ListData.getTimestamp());
+
+
+                }
+            }
     }
+/*for (List<Node> l1 : arrayLists) {
+   for (Node n : l1) {
+       System.out.print(n + " ");
+   }
+
+   System.out.println();
+} */
+
     public void push(){
       /*  dbManager = new DbManager(this);
         dbManager.open();
--- atma-client/src/main/java/tgwofficial/atma/client/sync/model/IbuModel/IdentitasIbuModel.java
+++ atma-client/src/main/java/tgwofficial/atma/client/sync/model/IbuModel/IdentitasIbuModel.java
@@ -3,6 +3,8 @@ package tgwofficial.atma.client.sync.model.IbuModel;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 
 public class IdentitasIbuModel {
 
