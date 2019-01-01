d769693f4cbd0798cd65d85046305e6dd247055a
mib-iqbal <mib.iqbaal@gmail.com>
2019-01-01 09:18:10 +0800

    MI | hide sync button except ibu activity

--- atma-client/src/main/java/theseinitiatives/atma/client/activity/KaderActivity.java
+++ atma-client/src/main/java/theseinitiatives/atma/client/activity/KaderActivity.java
@@ -156,7 +156,7 @@ public class KaderActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-
+        menu.setGroupVisible(0,false);
         return true;
     }
 
